06ffee1b84cc4f962aa08e5658651fcf104565ca
VinayakX <rasgotravinayak@gmail.com>
2025-08-20 00:43:58 +0530

    d

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base:import.meta.env.VITE_BASE_PATH|| "/react-vite-deploy",
 })
